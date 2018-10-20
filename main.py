7139b8ed55f4f8f1842648ddb3cb67bf6bb54902
Dominick Dufner <dominickdufner@yahoo.com>
2018-10-19 19:04:37 -0500

    final working code that i submitted 10/19/18

--- main.py
+++ main.py
@@ -8,78 +8,58 @@ app.config['SQLALCHEMY_ECHO'] = True
 db = SQLAlchemy(app) 
 app.secret_key='RR247a'
 
-class Blog(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    title = db.Column(db.String(120))
-    entry = db.Column(db.Text)
+class Blog(db.Model): 
+    id = db.Column(db.Integer, primary_key=True) 
+    title = db.Column(db.String(120)) 
+    entry = db.Column(db.Text) 
 
-    def __init__(self, title, entry): #check on how to pass through multiple things
+    def __init__(self, title, entry): 
         self.title = title
         self.entry = entry
-        #self.owner=owner
-
-#blogs =[]
-
-@app.route('/', methods=['POST', 'GET'])
-def index():
-    #below might need to be redone to direct between pages.  
-    if request.method == 'POST':
-        new_blog = request.form['blog_title']
-        new_blog = Blog(new_blog)
-        db.session.add(new_blog)
-        db.session.commit()
-    blogs = Blog.query.all()
-    return render_template('blog.html', name="blog_title", blogs=blogs) #add part to render blog posts) 
-    # blogs = Blog.query.filter_by(completed=False).all() 
-    # completed_blogs = Blog.query.filter_by(completed=True).all()
-   # return render_template('todos.html',title="Get It Done!", 
-       # tasks=tasks, completed_tasks=completed_tasks)
-
-# @app.route('/blog/<int:blog_id>') #default method is GET
-# def blog(blog_id):
-#     blog=Blog.query.filter_by(id=blog_id).one()
-#     return render_template('blog.html', blog=blog)
 
 @app.route("/", methods=["GET"])
-def return_to_main():
-    return render_template ('/blog')
+def index():
+    return redirect('/blog') 
+ 
+@app.route("/blog", methods=["GET"])
+def single_blog():
+    #queries all blogs 
+    all_blogs=Blog.query.all() 
 
-@app.route('/newpost', methods=["POST"])
-def newpost():
-    title=request.form["blog_title"]
-    entry=request.form["blog_entry"]
-    new_blog_template = jinja_env.get_template ("newpost.html")
+    #if a request if made for a specific blog id, bring up single blog post
+    blog_id = request.args.get('id') 
+    if blog_id: 
+        single_blog = Blog.query.get(blog_id)
+        return render_template('ind_blog_post.html', title=single_blog.title, entry=single_blog.entry) 
+    
+    return render_template('blog.html', blogs=all_blogs) #title="Build A Blog", posts=Blog.query.filter_by().all())
+  
+@app.route("/newpost", methods=["POST", "GET"]) 
+def newpost(): 
     if request.method == 'POST':
-        if title =="":
-            blog_title_error="Please enter title for blog post"
-            return new_blog_template.render()
-        else:
-            entry ==""
-            blog_entry_error="Please make blog entry"  
-            return new_blog_template.render()
-    else:
-        return new_blog_template.render()  
+        title=request.form["blog_title"]
+        entry=request.form["blog_entry"]
         
-    post=Blog(blog_title=blog_title, blog_date=blog_date, blog_entry=blog_entry)
+        if title =="": 
+            flash("Please enter title for blog post", "error")
+            return render_template("newpost.html") 
+        if entry=="":
+            flash("Please create entry for blog", "error") 
+            return render_template("newpost.html") 
+            #terminating after this else if
+            # entry ==""
+            # flash("Please make blog entry", "error") 
 
-    db.session.add(post)
-    db.session.commit()
-    return redirect(url_for('blog')) ## 
-    #return '<h1>title: {} date: {} entry: {}</h1>'.format(title, date, entry) 
-    #return render_template('newpost.html')
+        else:
+        # needs to not send to DB if incorrect.  
+            full_blog = Blog(title, entry)
+            db.session.add(full_blog)
+            db.session.commit()
 
-#NOTE: below is just for learning/ref, not needed
-@app.route('/delete_blog', methods=["POST"])
-def delete_blog():
-    blog_id = int(request.form["blog-id"])
-    blog =Blog.query.get(blog_id)
-    db.session.delete(blog)
-    db.session.commit()
-    return redirect('/')
-# @app.route('/', methods=["GET", "POST"])
-# def index(): 
-#     blog = Blog.query.all()
-#     return redirect ("/blog")#render_template('blog.html') #last error 
+            url= "/blog?=" + str(full_blog.id)                    
+            return redirect(url) #??? adds index to each blog post
+    
+    return render_template ("newpost.html") 
 
 if __name__ == '__main__':
     app.run()
