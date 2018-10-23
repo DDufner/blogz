1f56f8a8f7954195f95001ebdd67b8d89f3e9ea0
Dominick Dufner <dominickdufner@yahoo.com>
2018-10-22 19:55:51 -0500

    log in and sign up working, but not sending info to database

--- main.py
+++ main.py
@@ -12,44 +12,38 @@ class Blog(db.Model):
     id = db.Column(db.Integer, primary_key=True) 
     title = db.Column(db.String(120)) 
     entry = db.Column(db.Text) 
-    owner_id = db.Column(db.Integer, db.ForeignKey('user.id')) ######double check 
+    owner_id = db.Column(db.Integer, db.ForeignKey('user.id')) ######double check 'user.id' if issues
 
-    def __init__(self, title, entry): 
+    def __init__(self, title, entry, owner_id): 
         self.title = title
         self.entry = entry
-        self.owner = owner
+        self.owner_id = owner_id
 
 class User(db.Model):
         id = db.Column(db.Integer, primary_key=True) 
         username = db.Column(db.String(120))
         password = db.Column(db.String(20))
-        blogs = db.relationship('Blog', backref='owner')
+        blogs = db.relationship('Blog', backref='user') #was 'user.id'
 
-    def __init__(self, username, password): #got error, deleted indentation?
-        self.username = username
-        self.password = password
+def __init__(self, username, password): #got error so i deleted indention?
+    self.username = username
+    self.password = password
 
-#### Still need to initialize tables, #####
-#### ! getting error when i try to 'db.create_all()'
-
-
-@app.route("/", methods=["GET"]) #double check
+@app.route("/", methods=["GET"])
 def index():
     return redirect('/blog') 
  
-# @app.route("/blog", methods=["GET"])
-# def single_blog():
-#     #queries all blogs 
-#     all_blogs=Blog.query.all() 
-
-    #if a request if made for a specific blog id, bring up single blog post
-    blog_id = request.args.get('id') 
-    if blog_id: 
-        single_blog = Blog.query.get(blog_id)
-        return render_template('ind_blog_post.html', title=single_blog.title, entry=single_blog.entry) 
+@app.route("/index", methods=["GET"])
+def authors():
+    authors=User.query.all() 
+    author_id = request.args.get('id') 
+    if author_id: 
+        author_blog = Blog.query.get(author_id)
+        return render_template('index.html', title=author_blog.title, entry=author_blog.entry) 
     
-    return render_template('blog.html', blogs=all_blogs) #title="Build A Blog", posts=Blog.query.filter_by().all())
-  
+    # return render_template('blog.html', blogs=all_blogs) #title="Build A Blog", posts=Blog.query.filter_by().all())
+
+
 @app.route('/signup', methods=['POST', 'GET'])
 def signup():
     if request.method == 'POST':
@@ -57,7 +51,7 @@ def signup():
         password = request.form['password']
         verify = request.form['verify']
         existing_user = User.query.filter_by(username=username).first()
-        if username = existing_user: #might not be correct syntax 
+        if username == existing_user: #might not be correct syntax 
             flash ("User name already exists")
             return render_template('/signup') 
         elif len(username) < 3: 
@@ -72,50 +66,53 @@ def signup():
         elif len(password) > 20:
             flash ("Must be less than 20 characters")
             return render_template('/signup')   
-        elif not password==confirmation:
+        elif not password==verify:
             flash ("Password confirmation does not match password")
             return render_template('/signup') 
-        
-        if not existing_user:
-            new_user = User(username, password)
+        else: #HAD not existing_user:
+            new_user = User() #HAD: passed through username, password
             db.session.add(new_user)
             db.session.commit()
             session['username'] = username 
-            return redirect('/newpost')
-        else:
-            # TODO - user better response messaging
-            return "<h1>Duplicate user</h1>"
-
-    return render_template('signup.html') ##
-
+            url="/index?=" +str(new_user.id)
+            return redirect(url)  #return redirect('/login')
+        # else:
+        #     return "<h1>Duplicate user</h1>"
+ 
+    return render_template ("signup.html") 
 
 @app.route("/login", methods=["POST", "GET"])
 def login():
     if request.method=="POST":
         username=request.form['username']
         password=request.form['password']
-        user = User.query.filter_by(username=username).first()
+        user = User.query.filter_by(username=username).first() #double check this query, maybe not filter by?  
         if user and user.password == password:
             session['username'] = username
             flash('logged in')
             return redirect("/newpost") 
-        elif not user == user.username:
-            flash('user does not exist', 'error')
+        elif user is None:
+            flash ('enter a user name', 'error')
             return redirect("/login")
-        elif not password == user.password:
+        if user is not None and user.username==request.form['username']: #had 'ERROR NoneType' object has no attribute 'username'
+            flash('user already exists', 'error')
+            return redirect("/login")
+        if password is not None and password != user.password: 
             flash('password is not correct', 'error')
             return redirect("/login")
     return render_template("login.html")
 
 @app.before_request
 def require_login():
-    allowed_routes = ['login','signup', 'index', 'list_blogs'] #double check 'list_blogs'
+    allowed_routes = ['login','signup', 'index', 'newpost'] #double check 'list_blogs'
     if request.endpoint not in allowed_routes and 'username' not in session:
         return redirect('/login') 
 
-@app.route() ##
+@app.route('/logout') # not clearing input lines 
 def logout():
     del session['username']
+    username=''
+    password=''
     return redirect ("/blog")
 
 @app.route("/newpost", methods=["POST", "GET"]) 
@@ -123,24 +120,22 @@ def newpost():
     if request.method == 'POST':
         title=request.form["blog_title"]
         entry=request.form["blog_entry"]
+        owner_id=request.form["blog_owner_id"]
         
         if title =="": 
             flash("Please enter title for blog post", "error")
             return render_template("newpost.html") 
+
         if entry=="":
             flash("Please create entry for blog", "error") 
             return render_template("newpost.html") 
-            #terminating after this else if
-            # entry ==""
-            # flash("Please make blog entry", "error") 
 
         else:
-        # needs to not send to DB if incorrect.  
-            full_blog = Blog(title, entry)
+            full_blog = Blog(title, entry, owner_id) #3rd arg was 'owner_id'
             db.session.add(full_blog)
             db.session.commit()
 
-            url= "/blog?=" + str(full_blog.id)                    
+            url= "/blog?=" + str(full_blog.id) ###                 
             return redirect(url) #??? adds index to each blog post
     
     return render_template ("newpost.html") 
